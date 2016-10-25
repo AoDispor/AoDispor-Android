affe9391b94f75faefecdec864fda5a69c6cf62d
PedroRomanoBarbosa <pdrobarbosa@gmail.com>
2016-10-25 21:23:34 +0100

    Main activity

--- app/src/main/java/pt/aodispor/aodispor_android/MainActivity.java
+++ app/src/main/java/pt/aodispor/aodispor_android/MainActivity.java
@@ -1,13 +1,32 @@
 package pt.aodispor.aodispor_android;
 
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    private TabPagerAdapter mSectionsPagerAdapter;
+    private MyViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mSectionsPagerAdapter = new TabPagerAdapter(getSupportFragmentManager());
+
+        mViewPager = (MyViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+    }
+
+    public MyViewPager getViewPager(){
+        return mViewPager;
     }
 }

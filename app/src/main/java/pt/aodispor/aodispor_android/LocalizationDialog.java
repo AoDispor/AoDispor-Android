48e4a860db54cfefb7eaa7f54a88fcab2a5c779e
JoaoPere <joaopedrosp@gmail.com>
2016-11-14 16:00:37 +0000

    Changed Listener to make the request after 7 chars entered (small delay on 7th digit entered)

--- app/src/main/java/pt/aodispor/aodispor_android/LocalizationDialog.java
+++ app/src/main/java/pt/aodispor/aodispor_android/LocalizationDialog.java
@@ -3,6 +3,7 @@ package pt.aodispor.aodispor_android;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
+import android.text.TextWatcher;
 import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -31,10 +32,10 @@ public class LocalizationDialog extends AlertDialog {
 
         TextView location = (TextView) findViewById(R.id.localizacao);
 
-        EditText.OnEditorActionListener listener = new ZipCodeOnEditText(location,_location,zip1,zip2);
+        TextWatcher listener = new ZipCodeOnEditText(location,_location,zip1,zip2);
 
-        zip1.setOnEditorActionListener(listener);
-        zip2.setOnEditorActionListener(listener);
+        zip1.addTextChangedListener(listener);
+        zip2.addTextChangedListener(listener);
     }
 
     public void setLocation(TextView location) {
--- app/src/main/java/pt/aodispor/aodispor_android/ZipCodeOnEditText.java
+++ app/src/main/java/pt/aodispor/aodispor_android/ZipCodeOnEditText.java
@@ -1,5 +1,7 @@
 package pt.aodispor.aodispor_android;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
@@ -13,7 +15,7 @@ import pt.aodispor.aodispor_android.API.CPPQueryResult;
 import pt.aodispor.aodispor_android.API.HttpRequestTask;
 import pt.aodispor.aodispor_android.API.SearchQueryResult;
 
-public class ZipCodeOnEditText implements EditText.OnEditorActionListener {
+public class ZipCodeOnEditText implements TextWatcher {
     private TextView _location;
     private TextView _location_reg;
 
@@ -28,37 +30,44 @@ public class ZipCodeOnEditText implements EditText.OnEditorActionListener {
     }
 
     @Override
-    public boolean onEditorAction(TextView textView, int actionId, KeyEvent event) {
-        if (actionId == EditorInfo.IME_ACTION_SEARCH ||
-                actionId == EditorInfo.IME_ACTION_DONE ||
-                event.getAction() == KeyEvent.ACTION_DOWN &&
-                        event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
-                                String zip1 = _zip1.getText().toString();
-                                String zip2 = _zip2.getText().toString();
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
-                                String url = "https://api.aodispor.pt/location/{cp4}/{cp3}";
+    }
 
-                                Log.d("FCC1",zip1);
-                                Log.d("FCC2",zip2);
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
-                                HttpRequestTask request = new HttpRequestTask(CPPQueryResult.class, null, url, zip1, zip2);
+    }
 
-                                CPPQueryResult result;
-                                try {
-                                    Log.d("CPPQuery","Executing GET REQUEST");
-                                    Log.d("STRING QUERY", url + zip1 + zip2);
-                                    result = (CPPQueryResult) request.execute().get(AppDefinitions.MILISECONDS_TO_TIMEOUT_ON_QUERY, TimeUnit.MILLISECONDS);
-                                } catch (Exception e) {
-                                    Log.d("L330:EXCP", e.toString());
-                                    return true;
-                                }
-                                if (result!=null && result.data!=null) {
-                                    _location.setText(result.data.getLocalidade());
-                                    _location_reg.setText(result.data.getLocalidade());
-                                }
+    @Override
+    public void afterTextChanged(Editable editable) {
+        String zip1 = _zip1.getText().toString();
+        String zip2 = _zip2.getText().toString();
 
-                                return true;
+        if(zip1.length() == 4 && zip2.length() == 3) {
+            String url = "https://api.aodispor.pt/location/{cp4}/{cp3}";
+
+            Log.d("FCC1",zip1);
+            Log.d("FCC2",zip2);
+
+            HttpRequestTask request = new HttpRequestTask(CPPQueryResult.class, null, url, zip1, zip2);
+
+            CPPQueryResult result;
+            try {
+                Log.d("CPPQuery","Executing GET REQUEST");
+                Log.d("STRING QUERY", url + zip1 + zip2);
+                result = (CPPQueryResult) request.execute().get(AppDefinitions.MILISECONDS_TO_TIMEOUT_ON_QUERY, TimeUnit.MILLISECONDS);
+            } catch (Exception e) {
+                Log.d("L330:EXCP", e.toString());
+                return;
+            }
+            if (result!=null && result.data!=null) {
+                _location.setText(result.data.getLocalidade());
+                _location_reg.setText(result.data.getLocalidade());
+            }
         }
-        return false; // pass on to other listeners.
+        else {
+            Log.d("L","Postal code yet to fill");
+        }
     }
 }

3de26111eb27a7c42b04a9e77fff38fded979282
PedroRomanoBarbosa <pdrobarbosa@gmail.com>
2016-11-18 14:06:39 +0000

    Fixed loading bug

--- app/src/main/java/pt/aodispor/aodispor_android/ProfileFragment.java
+++ app/src/main/java/pt/aodispor/aodispor_android/ProfileFragment.java
@@ -325,12 +325,11 @@ public class ProfileFragment extends Fragment implements HttpRequest, DialogCall
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        startLoading();
         if (resultCode == RESULT_OK && requestCode == SELECT_PICTURE && data != null) {
+            startLoading();
             Bundle bundle = data.getExtras();
             Bitmap image = bundle.getParcelable("data");
 
-            //TODO PUT image
             HttpRequestTask request = new HttpRequestTask(Professional.class, this, URL_UPLOAD_IMAGE);
             request.setMethod(HttpRequestTask.PUT_REQUEST);
             request.setType(HttpRequest.UPDATE_PROFILE);

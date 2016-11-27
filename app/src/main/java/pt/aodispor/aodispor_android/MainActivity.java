15881629660c9be67c5a5abab0a85f61457d9edb
Bruno Madeira <bru_15scp@hotmail.com>
2016-11-27 23:56:34 +0000

    US login completed (?)

--- app/src/main/java/pt/aodispor/aodispor_android/MainActivity.java
+++ app/src/main/java/pt/aodispor/aodispor_android/MainActivity.java
@@ -1,16 +1,14 @@
 package pt.aodispor.aodispor_android;
 
-import android.content.ContentResolver;
-import android.content.Intent;
-import android.database.Cursor;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.graphics.Typeface;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.telephony.SmsMessage;
-import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
@@ -26,6 +24,7 @@ import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
  */
 public class MainActivity extends AppCompatActivity {
     private MyViewPager mViewPager;
+    private static final boolean SKIP_LOGIN=false;
 
     /**
      * This method is called when the main activity is created.
@@ -36,7 +35,70 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         installFonts();
+        if(!SKIP_LOGIN) loginDialog();
+        else            startPagerAndMainContent();
+    }
 
+    /**
+     * Returns this activity's custom ViewPager.
+     * @return the custom ViewPager.
+     */
+    public MyViewPager getViewPager(){
+        return mViewPager;
+    }
+
+
+
+    public void installFonts() {
+        AppDefinitions.dancingScriptRegular = Typeface.createFromAsset(getAssets(),"fonts/dancing-script-ot/DancingScript-Regular.otf");
+
+        AppDefinitions.yanoneKaffeesatzBold = Typeface.createFromAsset(getAssets(),"fonts/Yanone-Kaffeesatz/YanoneKaffeesatz-Bold.otf");
+        AppDefinitions.yanoneKaffeesatzLight = Typeface.createFromAsset(getAssets(),"fonts/Yanone-Kaffeesatz/YanoneKaffeesatz-Light.otf");
+        AppDefinitions.yanoneKaffeesatzRegular = Typeface.createFromAsset(getAssets(),"fonts/Yanone-Kaffeesatz/YanoneKaffeesatz-Regular.otf");
+        AppDefinitions.yanoneKaffeesatzThin = Typeface.createFromAsset(getAssets(),"fonts/Yanone-Kaffeesatz/YanoneKaffeesatz-Thin.otf");
+    }
+
+    private void loginDialog()
+    {
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.phonenumber_request);
+        dialog.setTitle("LOGIN");
+        dialog.setCancelable(false);
+        dialog.setCanceledOnTouchOutside(false);
+        Button loginButton= (Button) dialog.findViewById(R.id.button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+                validationDialog();
+            }
+        });
+
+        dialog.show();
+
+    }
+
+    private void validationDialog()
+    {
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.sms_validation);
+        dialog.setTitle("LOGIN");
+        dialog.setCancelable(false);
+        dialog.setCanceledOnTouchOutside(false);
+        Button loginButton= (Button) dialog.findViewById(R.id.button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+                startPagerAndMainContent();
+            }
+        });
+
+        dialog.show();
+    }
+
+    private void startPagerAndMainContent()
+    {
         ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(this).build();
         ImageLoader.getInstance().init(config);
 
@@ -51,20 +113,4 @@ public class MainActivity extends AppCompatActivity {
         mViewPager.setCurrentItem(1);
     }
 
-    /**
-     * Returns this activity's custom ViewPager.
-     * @return the custom ViewPager.
-     */
-    public MyViewPager getViewPager(){
-        return mViewPager;
-    }
-
-    public void installFonts() {
-        AppDefinitions.dancingScriptRegular = Typeface.createFromAsset(getAssets(),"fonts/dancing-script-ot/DancingScript-Regular.otf");
-
-        AppDefinitions.yanoneKaffeesatzBold = Typeface.createFromAsset(getAssets(),"fonts/Yanone-Kaffeesatz/YanoneKaffeesatz-Bold.otf");
-        AppDefinitions.yanoneKaffeesatzLight = Typeface.createFromAsset(getAssets(),"fonts/Yanone-Kaffeesatz/YanoneKaffeesatz-Light.otf");
-        AppDefinitions.yanoneKaffeesatzRegular = Typeface.createFromAsset(getAssets(),"fonts/Yanone-Kaffeesatz/YanoneKaffeesatz-Regular.otf");
-        AppDefinitions.yanoneKaffeesatzThin = Typeface.createFromAsset(getAssets(),"fonts/Yanone-Kaffeesatz/YanoneKaffeesatz-Thin.otf");
-    }
 }

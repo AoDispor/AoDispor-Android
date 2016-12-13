52b8a74c6e8424b816adccec5102b6445fb9f14f
Bruno Madeira <bru_15scp@hotmail.com>
2016-12-13 19:31:41 +0000

    text on voice search & .yml (but no tests)

--- app/src/main/java/pt/aodispor/aodispor_android/MainActivity.java
+++ app/src/main/java/pt/aodispor/aodispor_android/MainActivity.java
@@ -9,7 +9,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageView;
 
